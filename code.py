44d508952d4aa6aa20e53f2e1206fa645ad99303
Loreto Dumitrescu <loretodalt@gmail.com>
2021-12-31 17:13:24 -0500

    timing changes
    Update code to improve timing when pause and resume are triggered.

--- code.py
+++ code.py
@@ -1,26 +1,30 @@
 """
 CircuitPython single MP3 playback example for Raspberry Pi Pico.
 Plays a single MP3 once.
+Sources:https://learn.adafruit.com/adafruit-max98357-i2s-class-d-mono-amp/circuitpython-wiring-test
+Adapted: Loreto Dumitrescu
 """
 import board
 import audiomp3
 import audiopwmio
 import time
 from digitalio import DigitalInOut, Direction, Pull
+import audiocore
+import audiobusio
 import neopixel
 
 
 #Instance of audio output and sound file
-audio = audiopwmio.PWMAudioOut(board.A0)
+audio = audiobusio.I2SOut(board.TX, board.RX, board.D9)
 decoder = audiomp3.MP3Decoder(open("happy.mp3", "rb"))
 
 #Instance of drawing pad
-pad = DigitalInOut(board.A1)
+pad = DigitalInOut(board.A0)
 pad.direction = Direction.INPUT
 pad.pull = Pull.UP
 
 #Instance of stop button
-stop_button = DigitalInOut(board.A2)
+stop_button = DigitalInOut(board.A1)
 stop_button.direction = Direction.INPUT
 stop_button.pull = Pull.UP
 
@@ -41,19 +45,19 @@ def pause_audio(audio, pixel):
     print("audio pause")
     pixel.fill((0,0,255,))
     paused = True
-    time.sleep(.5)
+    time.sleep(.1)
 
 def resume_audio(audio, pixel):
     audio.resume()
     print("audio resume")
     pixel.fill((0,255,0,))
-    time.sleep(1)
+    time.sleep(.1)
 
 def stop_audio(audio, pixel):
     audio.stop()
     print("audio stopped")
     pixel.fill((255,0,0,))
-    time.sleep(1)
+    time.sleep(.1)
 
 def handle_press(audio, pixel, decoder, paused):
     if not audio.playing: # the first touch
@@ -62,6 +66,7 @@ def handle_press(audio, pixel, decoder, paused):
     else:
         resume_audio(audio,pixel)
 
+print('get the music started!')
 while True:
     if not pad.value:
         handle_press(audio, pixel, decoder, paused)
